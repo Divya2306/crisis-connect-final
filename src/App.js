ba64d817c3d39fd918eda8ea07cdc2d9420dae43
Dhruv <dhruv.gupta1873@gmail.com>
2023-04-21 15:40:49 +0530

    Added category component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+
+      <Categories />
     </div>
   );
 }
