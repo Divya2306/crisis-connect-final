082159fea8a2e356fb629c0f556ffad37314cbb5
Divya2306 <96518035+Divya2306@users.noreply.github.com>
2023-04-23 11:21:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Blog from "./components/Blog";
+import Mainpage from "./components/Mainpage";
 import Footer from "./components/footer";
+import Emergencies from "./components/Emergencies";
+import Helpline1 from "./components/Helpline1";
+import Helpline2 from "./components/Helpline2";
+import Helpline3 from "./components/Helpline3";
+import Helpline4 from "./components/Helpline4";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 
@@ -23,6 +29,12 @@ function App() {
             <Route path="/aboutus" element={<About />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/footer" element={<Footer />} />
+            <Route path="/mainpage" element={<Mainpage />} />
+            <Route path="/emergency" element={<Emergencies />} />
+            <Route path="/helpline1" element={<Helpline1 />} />
+            <Route path="/helpline2" element={<Helpline2 />} />
+            <Route path="/helpline3" element={<Helpline3 />} />
+            <Route path="/helpline4" element={<Helpline4 />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
